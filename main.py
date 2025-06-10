a79fc0245187aa41314de9707009bc3ca9995cc4
Gabriel Fincham <Gabe.fincham@gmail.com>
2025-06-10 21:36:26 +1000

    Replaced Classes
    - Removed classes for functions as things were not working such as button controls

--- main.py
+++ main.py
@@ -4,70 +4,95 @@ from ui.homepage import Ui_HomeWindow
 from PySide6.QtWidgets import QApplication, QMainWindow
 from src.backend.user_manager import login, signup
 
-class LoginPage:
-    def __init__(self, main_window):
-        self.log = Ui_LoginWindow()
-        self.log.setupUi(main_window)
-        self.log.SignupButton.clicked.connect(self.click_button_signup)
-        self.log.LoginButton.clicked.connect(self.click_button_login)
-    
-    def login_notify(self, message):
-        self.log.NotifyLabel.setText(message)
-        
-    def click_button_signup(self):
-        inputusername = self.log.UsernameInput.text()
-        inputpassword = self.log.PasswordInput.text()
+# Global variables
+MainWindow = None
+logged_in_user = None
+
+def setup_login_page():
+    global MainWindow
+    login_ui = Ui_LoginWindow()
+    login_ui.setupUi(MainWindow)
+
+    def login_notify(message):
+        login_ui.NotifyLabel.setText(message)
+
+    def click_button_signup():
+        inputusername = login_ui.UsernameInput.text()
+        inputpassword = login_ui.PasswordInput.text()
         outcome = signup(inputusername, inputpassword)
         print(outcome)
         if outcome == (True, 'Account created successfully.'):
-            self.login_notify("Account Created, Please Log In")
+            login_notify("Account Created, Please Log In")
         elif outcome == (False, 'Username already exists.'):
-            self.login_notify("Username already exists")
+            login_notify("Username already exists")
         elif outcome == (False, 'Password must be at least 7 characters long.'):
-            self.login_notify("Password must be at least 7 characters long")
+            login_notify("Password must be at least 7 characters long")
         elif outcome == (False, 'Username cannot be empty.'):
-            self.login_notify("Username cannot be empty")
+            login_notify("Username cannot be empty")
         elif outcome == (False, 'Password cannot be empty.'):
-            self.login_notify("Password cannot be empty")
-    
-    def click_button_login(self):
-        inputusername = self.log.UsernameInput.text()
-        inputpassword = self.log.PasswordInput.text()
+            login_notify("Password cannot be empty")
+
+    def click_button_login():
+        global logged_in_user
+        inputusername = login_ui.UsernameInput.text()
+        inputpassword = login_ui.PasswordInput.text()
         outcome = login(inputusername, inputpassword)
         if outcome == (False, "Username not found."):
-            self.login_notify("Username not found")
+            login_notify("Username not found")
         elif outcome == (False, "Username does not exist"):
-            self.login_notify("Username does not exist")
+            login_notify("Username does not exist")
         elif outcome == (False, 'Incorrect password.'):
-            self.login_notify("Incorrect password")
+            login_notify("Incorrect password")
         elif outcome == (True, "Login successful."):
             print("Logged in as: " + inputusername)
-            #GO TO HOMEPAGE
-            self.log.centralwidget.hide()  # Hide the login page
-            HomePage(self.log.centralwidget.parent())  # Initialize the HomePage
-            
-            
-class HomePage:
-    def __init__(self, main_window):
-        print("boothomepage")
-        self.home = Ui_HomeWindow()
-        self.home.setupUi(main_window)
-        self.home.RecommendedButton.clicked.connect(self.click_recommendedButton)
-        self.home.RecommendedView.clicked.connect(self.click_recommendedView)
-        self.home.SearchButton.clicked.connect(self.click_searchButton)
-        self.home.SettingsButton.clicked.connect(self.click_settingsButton)
-        self.home.LogoutButton.clicked.connect(self.click_logoutButton)
-        self.home.ExitButton.clicked.connect(self.click_exitButton)
-        self.home.WishlistButton.clicked.connect(self.click_wishlistButton)
-        
-    
+            logged_in_user = inputusername
+            setup_homepage()
 
+    login_ui.SignupButton.clicked.connect(click_button_signup)
+    login_ui.LoginButton.clicked.connect(click_button_login)
 
+def setup_homepage():
+    global MainWindow, logged_in_user
+    MainWindow.setCentralWidget(None)  # Clear the previous UI
+    home_ui = Ui_HomeWindow()
+    home_ui.setupUi(MainWindow)
+    home_ui.WelcomeText.setText(u"<html><head/><body><p><span style=\" font-size:18pt;\">Welcome "+logged_in_user+"!</span></p></body></html>")
+
+    def click_recommended_button():
+        print("Recommended Button clicked")
+
+    def click_recommended_view():
+        print("Recommended View clicked")
+
+    def click_search_button():
+        print("Search Button clicked")
+
+    def click_settings_button():
+        print("Settings Button clicked")
+
+    def click_logout_button():
+        print("Logout Button clicked")
+        setup_login_page()  # Return to login page
+
+    def click_exit_button():
+        print("Exit Button clicked")
+        sys.exit()
+
+    def click_wishlist_button():
+        print("Wishlist Button clicked")
+
+    # Connect buttons to func
+    home_ui.RecommendedButton.clicked.connect(click_recommended_button)
+    home_ui.RecommendedView.clicked.connect(click_recommended_view)
+    home_ui.SearchButton.clicked.connect(click_search_button)
+    home_ui.SettingsButton.clicked.connect(click_settings_button)
+    home_ui.LogoutButton.clicked.connect(click_logout_button)
+    home_ui.ExitButton.clicked.connect(click_exit_button)
+    home_ui.WishlistButton.clicked.connect(click_wishlist_button)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     MainWindow = QMainWindow()
-    login_page = LoginPage(MainWindow)
-    
+    setup_login_page()
     MainWindow.show()
     sys.exit(app.exec())
